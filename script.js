666dfd877688af22f4d2ad3e59e81671f491f8cb
Rodolpho <rhbpinheiro30@gmail.com>
2022-04-19 22:05:34 -0300

    Fisrt commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const eye = document.querySelector('.img-eye');
+const password = document.querySelector('.password');
+const divEye = document.querySelector('.eye');
+
+
+password.oninput = () => {
+    let showEye = (password.value === '');
+    if (!showEye) {
+        divEye.style.display = 'block';
+    } else {
+        divEye.style.display = 'none';
+    }
+
+}
+
+eye.onclick = () => {
+    if(password.type === 'password') {
+        password.type = 'text';
+        eye.src = "./images/open-eye.svg";
+    } else {
+        password.type = 'password';
+        eye.src = "./images/closed-eye.svg";
+    }
+}
+
+
